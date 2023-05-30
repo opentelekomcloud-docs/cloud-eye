1a537f464bcea48a0188632b0ccc6ed2f0bd00a8
tischrei <tino.schreiber@t-systems.com>
2023-05-30 09:52:32 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- api-ref/source/conf.py
+++ api-ref/source/conf.py
@@ -28,6 +28,15 @@ project = 'Cloud Eye'
 otcdocs_repo_name = 'opentelekomcloud-docs/cloud-eye'
 # Those variables are required for edit/bug links
 
+# Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = 'public'
+otcdocs_doc_link = '/cloud-eye/api-ref/'
+otcdocs_doc_title = 'API Reference'
+otcdocs_doc_type = 'api-ref'
+otcdocs_service_category = 'md'
+otcdocs_service_title = 'Cloud Eye'
+otcdocs_service_type = 'ces'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
